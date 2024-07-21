4a1297f0d66c4b25872abaa1dc7423a21daac938
savashakan22 <savashakan22@gmail.com>
2024-07-22 00:07:26 +0300

    Revert "Merge branch 'Authentication' into main"
    This reverts commit b854e2f9f1bea6f4f7d610bfc6f13d5ee24b9449, reversing
    changes made to cf7cc3b4241ae26ea402eb710a7b329f34c1545c.

--- src/inroll/api/urls.py
+++ src/inroll/api/urls.py
@@ -1,10 +1,6 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, TestViewSet, QuestionViewSet, ChoiceViewSet, SubmissionViewSet, AnswerViewSet
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -16,6 +12,4 @@ router.register(r'answers', AnswerViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
