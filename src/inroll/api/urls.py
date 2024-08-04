6d5f8fa7322b07d254ca801017416144124a2117
aydngrkm <gorkemaydinn@gmail.com>
2024-08-04 16:45:49 +0300

    Update urls.py
    Co-Authored-By: Hakan Savaş <68226968+savashakan22@users.noreply.github.com>

--- src/inroll/api/urls.py
+++ src/inroll/api/urls.py
@@ -1,15 +1,34 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, TestViewSet, QuestionViewSet, ChoiceViewSet, SubmissionViewSet, AnswerViewSet
+from .views import (
+    CandidateViewSet,
+    RecruiterViewSet, 
+    TestViewSet, 
+    QuestionViewSet, 
+    ChoiceViewSet, 
+    SubmissionViewSet, 
+    AnswerViewSet,
+    CandidateAssignedTests,
+    RecruiterAvailableTests,
+    CandidateSubmissions,
+)
 
 router = DefaultRouter()
-router.register(r'users', UserViewSet)
 router.register(r'tests', TestViewSet)
 router.register(r'questions', QuestionViewSet)
 router.register(r'choices', ChoiceViewSet)
 router.register(r'submissions', SubmissionViewSet)
 router.register(r'answers', AnswerViewSet)
+router.register(r'candidates', CandidateViewSet)
+router.register(r'recruiters', RecruiterViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('candidates/<int:candidate_id>/assigned-tests/', CandidateAssignedTests.as_view(), name='candidate-assigned-tests'),
+    path('recruiters/<int:recruiter_id>/available-tests/', RecruiterAvailableTests.as_view(), name='recruiter-available-tests'),
+    path('candidates/<int:candidate_id>/submissions/', CandidateSubmissions.as_view(), name='candidate-submissions'),
 ]
+
+## TODO
+# Viewlar icin url eklenecek
+# viewlara format=none eklenecek
